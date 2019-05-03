23168669b472e28f4f69ae5e9d384c33d7fb48e2
Michael Williams <mjwilli8@umd.edu>
2019-05-03 00:28:51 -0400

    added simple queries for the names of ACES and UMD twitter accounts

--- analyze.py
+++ analyze.py
@@ -16,38 +16,49 @@ auth.set_access_token(access_token, access_token_secret)
 # Create API object
 api = tweepy.API(auth)
 
-########## Analyzing my timeline ##########
-# Get tweets from my timeline
-public_tweets = api.home_timeline()
-
-# Iterate through public_tweets
-for tweet in public_tweets:
-    print tweet.text
-
-########## Analyzing tweets from a specific user ##########
-# Pull tweets from the NY times
-name = "nytimes"
-
-# Number of tweets to Pull
-tweet_count = 20
-
-# Get tweets
-nytimes_tweets = api.user_timeline(id=name, count=tweet_count)
-
-# Iterate through tweets
-for tweet in nytimes_tweets:
-    print tweet.text
-
 ########## Finding Tweets via keyword ##########
-# Search for Tweets with keyword Trump
-query = "Trump"
-
-# Language code
-language = "en"
+print "----------------------------- ACES_UMD Query -----------------------------"
+# Search for Tweets with keyword ACES
+query = "ACES_UMD"
 
 # Search query
-search_results = api.search(q=query, lang=language)
+search_results = api.search(q=query,count=100)
 
 # Iterate through search_results
 for tweet in search_results:
     print tweet.user.screen_name,"Tweeted:",tweet.text
+print str(len(search_results)) + " total results"
+
+print "----------------------------- UofMaryland Query -----------------------------"
+# Search for Tweets with keyword UofMaryland
+query = "UofMaryland"
+
+# Search query
+search_results = api.search(q=query,count=100)
+
+# Iterate through search_results
+for tweet in search_results:
+    print tweet.user.screen_name,"Tweeted:",tweet.text
+print str(len(search_results)) + " total results"
+
+########## Analyzing my timeline ##########
+# Get tweets from my timeline
+#public_tweets = api.home_timeline()
+
+# Iterate through public_tweets
+#for tweet in public_tweets:
+#    print tweet.text
+
+########## Analyzing tweets from a specific user ##########
+# Pull tweets from the NY times
+#name = "nytimes"
+
+# Number of tweets to Pull
+#tweet_count = 20
+
+# Get tweets
+#nytimes_tweets = api.user_timeline(id=name, count=tweet_count)
+
+# Iterate through tweets
+#for tweet in nytimes_tweets:
+#    print tweet.text
