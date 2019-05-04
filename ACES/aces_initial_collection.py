055471c1e244632ee23725faf308da289944303f
Michael Williams <mjwilli8@umd.edu>
2019-05-04 19:04:42 -0400

    cleaned up aces initial collection. added data structure

--- ACES/aces_initial_collection.py
+++ ACES/aces_initial_collection.py
@@ -16,293 +16,48 @@ auth.set_access_token(access_token, access_token_secret)
 # Create API object
 api = tweepy.API(auth)
 
-########## Finding Tweets via keyword ##########
+# Create data structure to store data
+data = {}
+
+# @ACES_UMD Queries
+aces_umd_queries = ["@ACES_UMD", "@ACES_UMD :(", "@ACES_UMD :)", "@ACES_UMD:media", "@ACES_UMD:images", "@ACES_UMD:links"]
+
+# ACES + UMD Queries
+aces_plus_umd_queries = ["ACES UMD", "ACES UMD :(", "ACES UMD :)", "ACES UMD:media", "ACES UMD:images", "ACES UMD:links"]
+
+# ACES + Maryland Queries
+aces_maryland_queries = ["ACES Maryland", "ACES Maryland :(", "ACES Maryland :)", "ACES Maryland:media", "ACES Maryland:images", "ACES Maryland:links"]
+
+# ACES + CS Queries
+aces_cs_queries = ["ACES CS", "ACES CS :(", "ACES CS :)", "ACES CS:media", "ACES CS:images", "ACES CS:links"]
+
+########## Finding Tweets via keywords ##########
 print "-----------------------------------------------------------------------------"
 print "Searching for ACES related tweets..."
-print "----------------------------- @ACES_UMD query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD
-query = "@ACES_UMD"
+print "----------------------------- @ACES_UMD Queries -----------------------------"
+# Iterate through Queries, storing tweets in dictionary
+for query in aces_umd_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = search_results
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
-# Search query
-search_results = api.search(q=query,count=100)
+print "----------------------------- ACES + UMD Queries -----------------------------"
+# Iterate through Queries, storing tweets in dictionary
+for query in aces_plus_umd_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = search_results
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- @ACES_UMD negative query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD and a negative attitude
-query = "@ACES_UMD :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- @ACES_UMD positive query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD and a positive attitude
-query = "@ACES_UMD :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- @ACES_UMD media query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD and containing an image or video
-query = "@ACES_UMD:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- @ACES_UMD images query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD and containing an image
-query = "@ACES_UMD:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- @ACES_UMD url query -----------------------------"
-# Search for Tweets with keyword @ACES_UMD and containing a URL
-query = "@ACES_UMD:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD query -----------------------------"
-# Search for Tweets with keywords ACES and UMD
-query = "ACES UMD"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD negative query -----------------------------"
-# Search for Tweets with keywords ACES and UMD and a negative attitude
-query = "ACES UMD :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD positive query -----------------------------"
-# Search for Tweets with keywords ACES and UMD and a positive attitude
-query = "ACES UMD :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD media query -----------------------------"
-# Search for Tweets with keywords ACES and UMD and containing an image or video
-query = "ACES UMD:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD images query -----------------------------"
-# Search for Tweets with keywords ACES and UMD and containing an image
-query = "ACES UMD:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + UMD url query -----------------------------"
-# Search for Tweets with keywords ACES and UMD and containing a URL
-query = "ACES UMD:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland
-query = "ACES Maryland"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland negative query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland and a negative attitude
-query = "ACES Maryland :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland positive query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland and a positive attitude
-query = "ACES Maryland :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland media query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland and containing an image or video
-query = "ACES Maryland:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland images query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland and containing an image
-query = "ACES Maryland:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + Maryland url query -----------------------------"
-# Search for Tweets with keywords ACES and Maryland and containing a URL
-query = "ACES Maryland:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+print "----------------------------- ACES + Maryland Queries -----------------------------"
+# Iterate through Queries, storing tweets in dictionary
+for query in aces_maryland_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = search_results
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
 print "----------------------------- ACES + CS query -----------------------------"
-# Search for Tweets with keywords ACES and CS
-query = "ACES CS"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + CS negative query -----------------------------"
-# Search for Tweets with keywords ACES and CS and a negative attitude
-query = "ACES CS :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + CS positive query -----------------------------"
-# Search for Tweets with keywords ACES and CS and a positive attitude
-query = "ACES CS :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + CS media query -----------------------------"
-# Search for Tweets with keywords ACES and CS and containing an image or video
-query = "ACES CS:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + CS images query -----------------------------"
-# Search for Tweets with keywords ACES and CS and containing an image
-query = "ACES CS:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- ACES + CS url query -----------------------------"
-# Search for Tweets with keywords ACES and CS and containing a URL
-query = "ACES CS:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+# Iterate through Queries, storing tweets in dictionary
+for query in aces_cs_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = search_results
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
