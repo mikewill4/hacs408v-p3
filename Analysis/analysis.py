353568bff325666562e30e972eefeaac7bbb490c
Michael Williams <mjwilli8@umd.edu>
2019-05-05 14:56:15 -0400

    updated analysis tests for umd data

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,13 +1,27 @@
 import json
 
+# Load json from aces_data.json file
 with open("../ACES/aces_data.json", "r") as file:
-    data = json.load(file)
+    aces_data = json.load(file)
 
-# Check that queries have been loaded
-for key in data.keys():
+# Load json from umd_data.json file
+with open("../UMD/umd_data.json", "r") as file:
+    umd_data = json.load(file)
+
+# Check that ACES queries have been loaded
+for key in aces_data.keys():
     print key
 
-# Quick test that json was loaded and attributes can be accessed
-print data["ACES UMD"][0]["text"]
-print data["ACES UMD"][0]["country"]
-print data["ACES UMD"][0]["created_at"]
+# Check that UMD queries have been loaded
+for key in umd_data.keys():
+    print key
+
+# Quick test that ACES json was loaded and attributes can be accessed
+print aces_data["ACES UMD"][0]["text"]
+print aces_data["ACES UMD"][0]["country"]
+print aces_data["ACES UMD"][0]["created_at"]
+
+# Quick test that UMD json was loaded and attributes can be accessed
+print umd_data["@UofMaryland"][0]["text"]
+print umd_data["@UofMaryland"][0]["country"]
+print umd_data["@UofMaryland"][0]["created_at"]
