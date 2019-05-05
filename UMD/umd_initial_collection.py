d0e89c925a454cafcdce9a90f89bee277e616b86
Michael Williams <mjwilli8@umd.edu>
2019-05-05 14:52:15 -0400

    completed initial collection for umd tweets

--- UMD/umd_initial_collection.py
+++ UMD/umd_initial_collection.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 ########## Setting up API ##########
 # API keys
@@ -16,293 +17,102 @@ auth.set_access_token(access_token, access_token_secret)
 # Create API object
 api = tweepy.API(auth)
 
+# Create data structure to store data
+data = {}
+
+# @UofMaryland Queries
+uofmaryland_queries = ["@UofMaryland", "@UofMaryland :(", "@UofMaryland :)", "@UofMaryland:media", "@UofMaryland:images", "@UofMaryland:links"]
+
+# UMCP Queries
+umcp_queries = ["UMCP", "UMCP :(", "UMCP :)", "UMCP:media", "UMCP:images", "UMCP:links"]
+
+# university maryland college park Queries
+umd_college_park_queries = ["university maryland college park", "university maryland college park :(", "university maryland college park :)", "university maryland college park:media", "university maryland college park:images", "university maryland college park:links"]
+
+# university maryland cp Queries
+umd_cp_queries = ["university maryland cp", "university maryland cp :(", "university maryland cp :)", "university maryland cp:media", "university maryland cp:images", "university maryland cp:links"]
+
+# Utility method to add tweet fields to data dictionary
+def add_tweet_fields(query, tweet):
+    # Null checks
+    if tweet.place == None or tweet.coordinates == None:
+        data[query].append({
+            "id": tweet.id,
+            "created_at": str(tweet.created_at),
+            "text": tweet.text,
+            "user_name": tweet.user.screen_name,
+            "user_location": tweet.user.location,
+            "user_verified": tweet.user.verified,
+            "user_follower_count": tweet.user.followers_count,
+            "user_time_zone": tweet.user.time_zone,
+            "user_language": tweet.user.lang,
+            "country": None,
+            "country_code": None,
+            "place_full_name": None,
+            "coordinates": [],
+            "retweet_count": tweet.retweet_count,
+            "favorite_count": tweet.favorite_count,
+            "tweet_language": tweet.lang
+        })
+    else:
+        data[query].append({
+            "id": tweet.id,
+            "created_at": str(tweet.created_at),
+            "text": tweet.text,
+            "user_name": tweet.user.screen_name,
+            "user_location": tweet.user.location,
+            "user_verified": tweet.user.verified,
+            "user_follower_count": tweet.user.followers_count,
+            "user_time_zone": tweet.user.time_zone,
+            "user_language": tweet.user.lang,
+            "country": tweet.place.country,
+            "country_code": tweet.place.country_code,
+            "place_full_name": tweet.place.full_name,
+            "coordinates": tweet.coordinates,
+            "retweet_count": tweet.retweet_count,
+            "favorite_count": tweet.favorite_count,
+            "tweet_language": tweet.lang
+        })
+
 ########## Finding Tweets via keyword ##########
 print "-----------------------------------------------------------------------------"
 print "Searching for UMD related tweets..."
 print "----------------------------- UofMaryland Query -----------------------------"
-# Search for Tweets with keyword UofMaryland
-query = "UofMaryland"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UofMaryland negative Query -----------------------------"
-# Search for Tweets with keyword UofMaryland and a negative attitude
-query = "UofMaryland :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UofMaryland positive Query -----------------------------"
-# Search for Tweets with keyword UofMaryland and a positive attitude
-query = "UofMaryland :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UofMaryland media Query -----------------------------"
-# Search for Tweets with keyword UofMaryland and containing an image or video
-query = "UofMaryland:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UofMaryland media Query -----------------------------"
-# Search for Tweets with keyword UofMaryland and containing images
-query = "UofMaryland:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UofMaryland url Query -----------------------------"
-# Search for Tweets with keyword UofMaryland and containing links
-query = "UofMaryland:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+# Iterate through Queries, storing tweets in dictionary
+for query in uofmaryland_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = []
+    for tweet in search_results:
+        add_tweet_fields(query, tweet)
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
 print "----------------------------- UMCP Query -----------------------------"
-# Search for Tweets with keyword UMCP
-query = "UMCP"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UMCP negative Query -----------------------------"
-# Search for Tweets with keyword UMCP and a negative attitude
-query = "UMCP :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UMCP positive Query -----------------------------"
-# Search for Tweets with keyword UMCP and a positive attitude
-query = "UMCP :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UMCP media Query -----------------------------"
-# Search for Tweets with keyword UMCP and containing an image or video
-query = "UMCP:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UMCP media Query -----------------------------"
-# Search for Tweets with keyword UMCP and containing images
-query = "UMCP:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- UMCP url Query -----------------------------"
-# Search for Tweets with keyword UMCP and containing links
-query = "UMCP:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+# Iterate through Queries, storing tweets in dictionary
+for query in umcp_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = []
+    for tweet in search_results:
+        add_tweet_fields(query, tweet)
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
 print "----------------------------- university + maryland + college + park Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park
-query = "university maryland college park"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + college + park negative Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and a negative attitude
-query = "university maryland college park :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + college + park positive Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and a positive attitude
-query = "university maryland college park :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + college + park media Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and containing an image or video
-query = "university maryland college park:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + college + park media Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and containing images
-query = "university maryland college park:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + college + park url Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and containing links
-query = "university maryland college park:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+# Iterate through Queries, storing tweets in dictionary
+for query in umd_college_park_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = []
+    for tweet in search_results:
+        add_tweet_fields(query, tweet)
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
 print "----------------------------- university + maryland + cp Query -----------------------------"
-# Search for Tweets with keywords university + maryland + cp
-query = "university maryland cp"
+# Iterate through Queries, storing tweets in dictionary
+for query in umd_cp_queries:
+    search_results = api.search(q=query,count=100)
+    data[query] = []
+    for tweet in search_results:
+        add_tweet_fields(query, tweet)
+    print "Added " + str(len(search_results)) + " tweets from " + query + " query"
 
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + cp negative Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and a negative attitude
-query = "university maryland cp :("
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + cp positive Query -----------------------------"
-# Search for Tweets with keywords university + maryland + cp and a positive attitude
-query = "university maryland cp :)"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + cp media Query -----------------------------"
-# Search for Tweets with keywords university + maryland + college + park and containing an image or video
-query = "university maryland cp:media"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + cp media Query -----------------------------"
-# Search for Tweets with keywords university + maryland + cp and containing images
-query = "university maryland cp:images"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
-
-print "----------------------------- university + maryland + cp url Query -----------------------------"
-# Search for Tweets with keywords university + maryland + cp and containing links
-query = "university maryland cp:links"
-
-# Search query
-search_results = api.search(q=query,count=100)
-
-# Iterate through search_results
-for tweet in search_results:
-    print tweet.user.screen_name,"Tweeted:",tweet.text
-print str(len(search_results)) + " total results"
+# Export data to json file
+with open("umd_data.json", "w") as fp:
+    json.dump(data, fp)
