6427f31db53166332bb8e7c7d473ce9d1f337b1c
Michael Williams <mjwilli8@umd.edu>
2019-05-07 23:49:57 -0400

    started spatial analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,4 +1,7 @@
+# Libraries
 import json
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.basemap import Basemap
 
 # Load json from aces_data.json file
 with open("../ACES/aces_data.json", "r") as file:
@@ -8,20 +11,47 @@ with open("../ACES/aces_data.json", "r") as file:
 with open("../UMD/umd_data.json", "r") as file:
     umd_data = json.load(file)
 
-# Check that ACES queries have been loaded
-for key in aces_data.keys():
-    print key
+########## UMD Analysis ##########
 
-# Check that UMD queries have been loaded
-for key in umd_data.keys():
-    print key
+########## Spatial analysis ##########
+# Extract tweets that have location data
+umd_tweet_coordinates = []
+for query,tweets in umd_data.items():
+    for tweet in tweets:
+        if len(tweet["coordinates"]) > 0:
+            umd_tweet_coordinates.append(tweet)
+print len(umd_tweet_coordinates)
 
-# Quick test that ACES json was loaded and attributes can be accessed
-print aces_data["ACES UMD"][0]["text"]
-print aces_data["ACES UMD"][0]["country"]
-print aces_data["ACES UMD"][0]["created_at"]
+# Creating the map
+# Set dimension of figure
+#dpi=96
+#plt.figure(figsize=(2600/dpi, 1800/dpi), dpi=dpi)
 
-# Quick test that UMD json was loaded and attributes can be accessed
-print umd_data["@UofMaryland"][0]["text"]
-print umd_data["@UofMaryland"][0]["country"]
-print umd_data["@UofMaryland"][0]["created_at"]
+# Make map background
+#m = Basemap(llcrnrlon=-180, llcrnrlat=-65, urcrnrlon=180, urcrnrlat=80)
+#m.drawmapboundary(fill_color="#A6CAE0", linewidth=0)
+#m.fillcontinents(color="grey", alpha=0.3)
+#m.drawcoastlines(linewidth=0.1, color="white")
+
+# Set colors for each point depending on continent
+
+# Add points
+# m.scatter()
+
+# Copyright
+#plt.text(-170, -58, "Plot created with Python and the Basemap library", ha="left", va="bottom", size="9", color="#555555")
+
+# Save as png
+#plt.savefig("test_bubble_map_umd.png", bbox_inches="tight")
+
+########## Sentiment analysis ##########
+
+########## Social anlysis ##########
+
+########## ACES Analysis
+
+########## Spatial analysis ##########
+
+########## Sentiment analysis ##########
+
+########## Social anlysis ##########
