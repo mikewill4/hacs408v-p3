23a0ae8390b29062a2b7f67f986cad718aa38cc5
Michael Williams <mjwilli8@umd.edu>
2019-05-13 15:10:50 -0400

    completed social analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -3,6 +3,7 @@ import json
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
 
@@ -71,6 +72,36 @@ plt.title("Sentiment Analysis of UMD")
 plt.show(block=True)
 
 ########## Social anlysis ##########
+umd_tweet_users = {}
+most_retweets_umd = -1
+most_favorites_umd = -1
+for query,tweets in umd_data.items():
+    for tweet in tweets:
+        if tweet["user_name"] not in umd_tweet_users:
+            umd_tweet_users[tweet["user_name"]] = tweet["user_follower_count"]
+        if tweet["retweet_count"] > most_retweets_umd:
+            most_retweets_umd = tweet["retweet_count"]
+            most_retweeted_tweet_umd = tweet
+        if tweet["favorite_count"] > most_favorites_umd:
+            most_favorites_umd = tweet["favorite_count"]
+            most_favorited_tweet_umd = tweet
+
+# Most popular tweets
+print most_retweeted_tweet_umd["text"]
+print most_favorited_tweet_umd["text"]
+
+# 10 most popular users
+sorted_users = sorted(umd_tweet_users.items(), key=operator.itemgetter(1), reverse=True)
+top_users = {}
+for i in range(0,10):
+    top_users[sorted_users[i][0]] = sorted_users[i][1]
+y_pos = np.arange(10)
+plt.bar(y_pos, top_users.values(), align="center", alpha=0.5, width=0.25)
+plt.xticks(y_pos, top_users.keys())
+plt.ylabel("# of Followers")
+plt.xlabel("User")
+plt.title("Social Analysis of UMD")
+plt.show(block=True)
 
 ########## ACES Analysis ##########
 
@@ -85,7 +116,7 @@ for query,tweets in aces_data.items():
     if ":(" in query:
         negative_sentiment_aces.extend(aces_data[query])
 
-# Iniitalize values for plot
+# Initialize values for plot
 sentiment_objects = ["Positive", "Negative"]
 y_pos = np.arange(len(sentiment_objects))
 sentiment_values_aces = [len(positive_sentiment_aces), len(negative_sentiment_aces)]
@@ -99,3 +130,33 @@ plt.title("Sentiment Analysis of ACES")
 plt.show(block=True)
 
 ########## Social anlysis ##########
+aces_tweet_users = {}
+most_retweets_aces = -1
+most_favorites_aces = -1
+for query,tweets in aces_data.items():
+    for tweet in tweets:
+        if tweet["user_name"] not in aces_tweet_users:
+            aces_tweet_users[tweet["user_name"]] = tweet["user_follower_count"]
+        if tweet["retweet_count"] > most_retweets_aces:
+            most_retweets_aces = tweet["retweet_count"]
+            most_retweeted_tweet_aces = tweet
+        if tweet["favorite_count"] > most_favorites_aces:
+            most_favorites_aces = tweet["favorite_count"]
+            most_favorited_tweet_aces = tweet
+
+# Most popular tweets
+print most_retweeted_tweet_aces["text"]
+print most_favorited_tweet_aces["text"]
+
+# 10 most popular users
+sorted_users = sorted(aces_tweet_users.items(), key=operator.itemgetter(1), reverse=True)
+top_users = {}
+for i in range(0,10):
+    top_users[sorted_users[i][0]] = sorted_users[i][1]
+y_pos = np.arange(10)
+plt.bar(y_pos, top_users.values(), align="center", alpha=0.5, width=0.25)
+plt.xticks(y_pos, top_users.keys())
+plt.ylabel("# of Followers")
+plt.xlabel("User")
+plt.title("Social Analysis of ACES")
+plt.show(block=True)
