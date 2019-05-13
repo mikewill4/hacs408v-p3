9d4c69de042828a3dea9d2fcc8f1db2e63e0219b
Michael Williams <mjwilli8@umd.edu>
2019-05-13 14:06:19 -0400

    completed sentiment analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,5 +1,8 @@
 # Libraries
 import json
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
 
@@ -45,13 +48,54 @@ print len(umd_tweet_coordinates)
 #plt.savefig("test_bubble_map_umd.png", bbox_inches="tight")
 
 ########## Sentiment analysis ##########
+# Get positive and negative tweets
+positive_sentiment_umd = []
+negative_sentiment_umd = []
+for query,tweets in umd_data.items():
+    if ":)" in query:
+        positive_sentiment_umd.extend(umd_data[query])
+    if ":(" in query:
+        negative_sentiment_umd.extend(umd_data[query])
+
+# Iniitalize values for plot
+sentiment_objects = ["Positive", "Negative"]
+y_pos = np.arange(len(sentiment_objects))
+sentiment_values_umd = [len(positive_sentiment_umd), len(negative_sentiment_umd)]
+
+# Plot
+plt.bar(y_pos, sentiment_values_umd, align="center", alpha=0.5, width=0.25)
+plt.xticks(y_pos, sentiment_objects)
+plt.ylabel("# of Tweets")
+plt.xlabel("Sentiment")
+plt.title("Sentiment Analysis of UMD")
+plt.show(block=True)
 
 ########## Social anlysis ##########
 
-########## ACES Analysis
+########## ACES Analysis ##########
 
 ########## Spatial analysis ##########
 
 ########## Sentiment analysis ##########
+positive_sentiment_aces = []
+negative_sentiment_aces = []
+for query,tweets in aces_data.items():
+    if ":)" in query:
+        positive_sentiment_aces.extend(aces_data[query])
+    if ":(" in query:
+        negative_sentiment_aces.extend(aces_data[query])
+
+# Iniitalize values for plot
+sentiment_objects = ["Positive", "Negative"]
+y_pos = np.arange(len(sentiment_objects))
+sentiment_values_aces = [len(positive_sentiment_aces), len(negative_sentiment_aces)]
+
+# Plot
+plt.bar(y_pos, sentiment_values_aces, align="center", alpha=0.5, width=0.25)
+plt.xticks(y_pos, sentiment_objects)
+plt.ylabel("# of Tweets")
+plt.xlabel("Sentiment")
+plt.title("Sentiment Analysis of ACES")
+plt.show(block=True)
 
 ########## Social anlysis ##########
